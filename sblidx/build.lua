264ee7023d09cfd566cf3052a0843117f947d5dd
David Purton <dcpurton@marshwiggle.net>
2026-01-12 22:33:59 +1030

    Add final code, do and test for all three index variants

--- sblidx/build.lua
+++ sblidx/build.lua
@@ -7,3 +7,16 @@ stdengine    = "pdftex"
 checkdeps = { maindir .. "/bibleref-sbl" }
 checkengines = {"pdftex"}
 checkfiles = { "*.ist" }
+checkruns = 2
+
+function runtest_tasks(name, run)
+  if name == "sblidx26" then
+    if run == 1 then
+      return "biber " .. name
+    else
+      return ""
+    end
+  else
+      return ""
+  end
+end
