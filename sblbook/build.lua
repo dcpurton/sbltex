56d85a0a30514ea0ec15b4b3189793afbd1f9743
David Purton <dcpurton@marshwiggle.net>
2026-03-14 21:09:00 +1030

    Add sblbook class

---
+++ sblbook/build.lua
@@ -0,0 +1,28 @@
+-- Build configuration for sblbook
+
+module = "sblbook"
+maindir = ".."
+checkformat = "latex"
+checkengines = {"luatex"}
+checkdeps = {
+  maindir .. "/sblfonts",
+  maindir .. "/sblidx",
+  maindir .. "/bibleref-sbl"
+}
+typesetdemofiles = {"sbltex-book.tex"}
+typesetdeps = {
+  maindir .. "/sblfonts",
+  maindir .. "/sblidx",
+  maindir .. "/bibleref-sbl"
+}
+typesetsuppfiles = {"sbltex.bib"}
+typesetexe = "pdflatex"
+
+function typeset_demo_tasks()
+  typesetexe = "lualatex"
+
+  local current = os.getenv("INDEXSTYLE") or ""
+  os.setenv("INDEXSTYLE", abspath(localdir) .. os_pathsep .. current)
+
+  return 0
+end
