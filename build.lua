578f2163d95ef28ab064385dcf5625c5376901fb
David Purton <dcpurton@marshwiggle.net>
2026-03-17 01:01:41 +1030

    Set up version tagging and update

--- build.lua
+++ build.lua
@@ -1,6 +1,7 @@
 -- Build configuration for the sbltex bundle
 
 bundle = "sbltex"
+
 modules = {
   "bibleref-sbl",
   "sblfonts",
@@ -9,3 +10,29 @@ modules = {
   "sblreport",
   "sblbook"
 }
+
+tagfiles = {"**/*.dtx"}
+
+function update_tag(file, content, tagname, tagdate)
+  content = content:gsub(
+    "(\\Provides%w+%s*%b{}%s*%[)%d%d%d%d%-%d%d%-%d%d%s+v[%d%.%a]+",
+    "%1" .. tagdate .. " v" .. tagname
+  )
+
+  content = content:gsub(
+    "(\\ProvidesExpl%w+%s*%b{}%s*)(%b{})(%s*)(%b{})",
+    function(prefix, _date, ws, _ver)
+      return prefix
+        .. "{" .. tagdate .. "}"
+        .. ws
+        .. "{" .. tagname .. "}"
+    end
+  )
+
+  content = content:gsub(
+    "(\\date%{)%d%d%d%d%-%d%d%-%d%d%s+v[%d%.%a]+(%})",
+    "%1" .. tagdate .. " v" .. tagname .. "%2"
+  )
+
+  return content
+end
